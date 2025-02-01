3aef6c2f01e899b4d80833283df30f397e4a70b7
akram-abid <a.abid44778@gmail.com>
2025-02-01 17:12:01 +0100

    do: finish the add functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class Node{
     constructor(value){
         this.value = value;
-        this.next = this.next;
+        this.next = null;
     }
 }
 
@@ -10,3 +10,40 @@ class LinkedList{
         this.head = null;
     }
 }
+
+function append(value, list){
+    let pointer = list.head;
+    if(list.head == null){
+        list.head = new Node(value);
+    }else{
+        while(pointer.next != null){
+            console.log("hi gg",pointer.next, pointer.next != null);
+            console.log("i am in");
+            pointer = pointer.next;
+        }
+        pointer.next = new Node(value);
+    }
+}
+
+function preAppend(value, list){
+    if(list.head == null){
+        list.head = new Node(value);
+    }else{
+        const tmp = list.head;
+        list.head = new Node(value);
+        list.head.next = tmp;
+    }
+}
+let list = new LinkedList();
+
+append(2, list);
+console.log("it not false now")
+append(3, list);
+console.log(list);
+
+console.log("you should be in by now");
+append(4, list);
+
+preAppend(1, list),
+
+console.log(list);
